4a08337608ef4aeb0e4272b37a04a5ebb34ceb72
yolo <77111763+yoloeww@users.noreply.github.com>
2021-02-09 20:12:05 +0800

    Add files via upload

---
+++ 2021.1.29/2021.2.9.字符串的输入和输出.cpp
@@ -0,0 +1,23 @@
+# include <stdio.h>
+void show (char *a,int len)
+{   int i;
+   for(i=0;i<len;i++)
+   {
+     printf("%d\n",a[i]);
+   }
+}
+int main(void)
+{
+   char a[30],b[30],c[30],ch;
+  /* scanf("%s%s%s",a,b,c,&ch);
+   printf("%s%s%s",a,b,c,ch);
+   show (a,30);
+   return 0;*/
+/*	gets(a);
+	ch=getchar();
+	puts(a);
+	putchar(ch);  */
+   puts("haha");
+	return 0;
+ 
+}
